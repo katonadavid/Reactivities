aaf965c2ae064f7ec827ee1434a8a371b48ccc2d
David Katona <david.katona@dataformers.at>
2021-11-02 20:32:04 +0100

    added detail and create

---
+++ Application/Activities/Create.cs
@@ -0,0 +1,35 @@
+using System.Threading;
+using System.Threading.Tasks;
+using Domain;
+using MediatR;
+using Persistence;
+
+namespace Application.Activities
+{
+    public class Create
+    {
+        public class Command : IRequest
+        {
+            public Activity activity { get; set; }
+        }
+
+        public class Handler : IRequestHandler<Command>
+        {
+            private readonly DataContext _context;
+            public Handler(DataContext context)
+            {
+                _context = context;
+            }
+
+            public async Task<Unit> Handle(Command request, CancellationToken cancellationToken)
+            {
+                _context.Activities.Add(request.activity);
+
+                await _context.SaveChangesAsync();
+                
+                // This way we let mediatr know that the command has been processed, this equals returning nothing
+                return Unit.Value;
+            }
+        }
+    }
+}
