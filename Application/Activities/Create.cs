b5005a2b06aa5d2d7a5a84af1d69a72a49b4447d
David Katona <david.katona@dataformers.at>
2021-12-13 16:47:54 +0100

    Added infrastructure project

--- Application/Activities/Create.cs
+++ Application/Activities/Create.cs
@@ -1,10 +1,12 @@
 using System.Threading;
 using System.Threading.Tasks;
 using Application.Core;
+using Application.Interfaces;
 using AutoMapper;
 using Domain;
 using FluentValidation;
 using MediatR;
+using Microsoft.EntityFrameworkCore;
 using Persistence;
 
 namespace Application.Activities
@@ -28,19 +30,31 @@ namespace Application.Activities
         {
             private readonly DataContext _context;
             private readonly IMapper _mapper;
+            private readonly IUserAccessor _userAccessor;
 
-            public Handler(DataContext context, IMapper mapper)
+            public Handler(DataContext context, IMapper mapper, IUserAccessor userAccessor)
             {
+                _userAccessor = userAccessor;
                 _context = context;
                 _mapper = mapper;
             }
 
             public async Task<Result<Activity>> Handle(Command request, CancellationToken cancellationToken)
             {
+                var user = await _context.Users.FirstOrDefaultAsync(x => x.UserName == _userAccessor.GetUserName());
+
+                var attendee = new ActivityAttendee
+                {
+                    AppUser = user,
+                    Activity = request.Activity,
+                    IsHost = true
+                };
+
+                request.Activity.Attendees.Add(attendee);
                 var newActivity = _context.Activities.Add(_mapper.Map<Activity>(request.Activity));
                 var result = await _context.SaveChangesAsync() > 0;
 
-                if(!result) return Result<Activity>.Success(newActivity.Entity);
+                if (!result) return Result<Activity>.Success(newActivity.Entity);
                 return Result<Activity>.Success(newActivity.Entity);
 
             }
